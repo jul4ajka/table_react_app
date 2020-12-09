ee6caa3409cb8a8d791fc2f88670473680778c8d
jul4ajka <jul4ajka@gmail.com>
2020-12-09 16:57:23 +0200

    Base URL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  ConnectedRouter,
+} from 'react-router-dom'
 import { AuthProvider } from './config/AuthProvider'
 import PrivateRoute from './config/PrivateRoute'
 import Login from './components/Auth/Login/Login'
@@ -6,16 +10,21 @@ import SignUp from './components/Auth/SignUp/SignUp'
 import Table from './components/Table/Table'
 
 function App() {
+  const history = createHistory({
+    basename: process.env.PUBLIC_URL,
+  })
   return (
-    <AuthProvider>
-      <Router>
-        <div>
-          <PrivateRoute exact path='/' component={Table} />
-          <Route path='/login' component={Login} />
-          <Route path='/sign-up' component={SignUp} />
-        </div>
-      </Router>
-    </AuthProvider>
+    <ConnectedRouter history={history}>
+      <AuthProvider>
+        <Router>
+          <div>
+            <PrivateRoute exact path='/' component={Table} />
+            <Route path='/login' component={Login} />
+            <Route path='/sign-up' component={SignUp} />
+          </div>
+        </Router>
+      </AuthProvider>
+    </ConnectedRouter>
   )
 }
 
